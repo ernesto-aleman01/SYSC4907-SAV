b74245d7a2dcb36f7678c7238e754664f8d2bcb4
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2023-02-02 00:36:41 -0500

    Create bridge interface for simulator calls

---
+++ src/common/src/common/airsim_bridge.py
@@ -0,0 +1,52 @@
+import airsim
+import rospy
+from common.bridge import Bridge, CarControls
+from numpy import ndarray
+
+
+class AirSimBridge(Bridge):
+    """
+    Create a bridge to simplify mocking when testing, switching to different sim
+    """
+    def __init__(self):
+        host_ip = rospy.get_param('/host_ip')
+
+        self.client = airsim.CarClient(ip=host_ip)
+        self.client.confirmConnection()
+        self.client.enableApiControl(True)
+
+    def get_image(self, camera_id: int, image_type: int, pixels_as_float=False, compress=False):
+        return self.client.simGetImages(
+            [airsim.ImageRequest(camera_id, image_type, pixels_as_float, compress)])[0]
+
+    def write_image(self, filepath: str, img: ndarray):
+        airsim.write_png(filepath, img)
+
+    def set_controls(self, controls: CarControls):
+        airsim_controls = airsim.CarControls(controls.throttle, controls.steering, controls.brake)
+        self.client.setCarControls(airsim_controls)
+
+    def get_steering(self):
+        return self.client.getCarControls().steering
+
+    def get_throttle(self):
+        return self.client.getCarControls().throttle
+
+    def get_position(self):
+        return self.client.simGetGroundTruthKinematics("").position
+
+    def get_orientation(self):
+        return self.client.getCarState().kinematics_estimated.orientation
+
+    def create_binvox(self, position, x, y, z, res, of):
+        airsim_pos = airsim.Vector3r(position.x_val, position.y_val, position.z_val)
+        self.client.simCreateVoxelGrid(airsim_pos, x, y, z, res, of)
+
+    def get_lidar_point_cloud(self):
+        return self.client.getLidarData().point_cloud
+
+    def get_speed(self):
+        return self.client.getCarState().speed
+
+    def has_collided(self):
+        return self.client.simGetCollisionInfo().has_collided
---
+++ src/common/src/common/bridge.py
@@ -0,0 +1,81 @@
+from abc import ABC
+import numpy as np
+from typing import List
+
+_instance = None
+
+SCENE_IMAGE = 0
+DEPTH_IMAGE = 3
+SEGMENTATION_IMAGE = 5
+
+
+class CarControls:
+    def __init__(self, throttle=0.0, steering=0.0, brake=0.0):
+        self.throttle = throttle
+        self.steering = steering
+        self.brake = brake
+
+
+class Image:
+    image_data_uint8 = np.uint8(0)
+    image_data_float = 0.0
+    width = 0
+    height = 0
+
+
+class Vector:
+    def __init__(self, x_val=0.0, y_val=0.0, z_val=0.0):
+        self.x_val = x_val
+        self.y_val = y_val
+        self.z_val = z_val
+
+
+class Quaternion:
+    def __init__(self, x_val=0.0, y_val=0.0, z_val=0.0, w_val=1.0):
+        self.x_val = x_val
+        self.y_val = y_val
+        self.z_val = z_val
+        self.w_val = w_val
+
+
+class Bridge(ABC):
+    def get_image(self, camera_id: int, image_type: int, pixels_as_float=False) -> Image:
+        pass
+
+    def write_image(self, filepath: str, img: np.ndarray) -> None:
+        pass
+
+    def set_controls(self, controls: CarControls) -> None:
+        pass
+
+    def get_steering(self) -> float:
+        pass
+
+    def get_throttle(self) -> float:
+        pass
+
+    def get_position(self) -> Vector:
+        pass
+
+    def get_orientation(self) -> Quaternion:
+        pass
+
+    def create_binvox(self, position: Vector, x: int, y: int, z: int, res: float, of: str) -> None:
+        pass
+
+    def get_lidar_point_cloud(self) -> List[float]:
+        pass
+
+    def get_speed(self) -> float:
+        pass
+
+    def has_collided(self) -> bool:
+        pass
+
+
+def get_bridge() -> Bridge:
+    global _instance
+    if _instance is None:
+        from common.airsim_bridge import AirSimBridge
+        _instance = AirSimBridge()
+    return _instance
